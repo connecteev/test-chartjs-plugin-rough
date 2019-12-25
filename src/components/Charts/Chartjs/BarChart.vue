7a714eb86ed0c80df08b8f99cd8b52a8a793883b
Kunal Punjabi <kunalspunjabi@gmail.com>
2019-12-24 19:39:26 -0800

    attempt 2 still failed, comment out

--- src/components/Charts/Chartjs/BarChart.vue
+++ src/components/Charts/Chartjs/BarChart.vue
@@ -1,12 +1,15 @@
 <script>
 import { Bar } from "vue-chartjs";
+// import { Rough } from "roughjs";
 // import ChartRough from "chartjs-plugin-rough";
 
 export default {
   extends: Bar,
   props: ["data", "options"],
   // plugins: [ChartRough],
+  // plugins: [Rough, ChartRough],
   mounted() {
+    // this.addPlugin(Rough);
     // this.addPlugin(ChartRough);
     this.renderChart(this.data, this.options);
   }
