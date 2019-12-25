5d13aee6b5e5b89497b4aa5316f6352d71e8de51
Kunal Punjabi <kunalspunjabi@gmail.com>
2019-12-24 16:56:20 -0800

    refactor, add HorizontalBarChart.vue

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,19 @@
 <template>
   <div id="app">
     <h1>LineChart</h1>
-    <LineChart
-      :data="{
-        labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
-        ],
-        datasets: [
-          {
-            label: 'Data One',
-            borderColor: '#f87979',
-            backgroundColor: 'rgba(255, 0, 0, 0.2)',
-            data: [40, 39, 10, 40, 39, 80, 40]
-          },
-          {
-            label: 'Data Two',
-            borderColor: '#79f879',
-            backgroundColor: 'rgba(0, 255, 0, 0.2)',
-            data: [39, 10, 40, 39, 80, 100, 22]
-          }
-        ]
-      }"
-      :options="{
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                min: 0,
-                stepSize: 5
-              }
-            }
-          ]
-        }
-      }"
-    />
+    <LineChart :data="myLineData" :options="myLineOptions" />
 
     <hr />
     <h1>BarChart</h1>
-    <BarChart
-      :data="{
-        labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
-        ],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: [
-              '#ff6384',
-              '#36a2eb',
-              '#cc65fe',
-              '#ffce56',
-              'red',
-              'green',
-              'blue'
-            ],
-            data: [40, 39, 20, 40, 39, 80, 40]
-          }
-        ]
-      }"
-      :options="{
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                min: 0,
-                stepSize: 5
-              }
-            }
-          ]
-        }
-      }"
-    />
+    <BarChart :data="myBarData" :options="myBarOptions" />
+
+    <hr />
+    <h1>HorizontalBarChart</h1>
+    <HorizontalBarChart :data="myBarData" :options="myBarOptions" />
 
     <hr />
     <h1>PieChart</h1>
-    <PieChart
-      :data="{
-        labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
-        ],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: [
-              '#ff6384',
-              '#36a2eb',
-              '#cc65fe',
-              '#ffce56',
-              'red',
-              'green',
-              'blue'
-            ],
-            data: [40, 39, 10, 40, 39, 80, 40]
-          }
-        ]
-      }"
-      :options="{ responsive: true, maintainAspectRatio: false }"
-    />
+    <PieChart :data="myPieData" :options="myPieOptions" />
 
     <hr />
     <h1>PieChart - Multiple</h1>
@@ -170,64 +65,248 @@
 
     <hr />
     <h1>DoughnutChart</h1>
-    <DoughnutChart
-      :data="{
+    <DoughnutChart :data="myDoughnutData" :options="myDoughnutOptions" />
+
+    <hr />
+    <h1>BubbleChart</h1>
+    <BubbleChart :data="myBubbleData" :options="myBubbleOptions" />
+
+    <hr />
+    <h1>ScatterChart</h1>
+    <ScatterChart :data="myScatterData" :options="myScatterOptions" />
+  </div>
+</template>
+
+<script>
+import LineChart from "./components/Charts/Chartjs/LineChart.vue";
+import BarChart from "./components/Charts/Chartjs/BarChart.vue";
+import HorizontalBarChart from "./components/Charts/Chartjs/HorizontalBarChart.vue";
+import PieChart from "./components/Charts/Chartjs/PieChart.vue";
+import DoughnutChart from "./components/Charts/Chartjs/DoughnutChart.vue";
+import BubbleChart from "./components/Charts/Chartjs/BubbleChart.vue";
+import ScatterChart from "./components/Charts/Chartjs/ScatterChart.vue";
+
+export default {
+  name: "app",
+  components: {
+    LineChart,
+    BarChart,
+    HorizontalBarChart,
+    PieChart,
+    DoughnutChart,
+    BubbleChart,
+    ScatterChart
+  },
+  data() {
+    return {
+      myLineData: {
         labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
         ],
         datasets: [
           {
-            label: 'Data One',
+            label: "Data One",
+            borderColor: "#f87979",
+            backgroundColor: "rgba(255, 0, 0, 0.2)",
+            data: [40, 39, 10, 40, 39, 80, 40]
+          },
+          {
+            label: "Data Two",
+            borderColor: "#79f879",
+            backgroundColor: "rgba(0, 255, 0, 0.2)",
+            data: [39, 10, 40, 39, 80, 100, 22]
+          }
+        ]
+      },
+      myLineOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                stepSize: 5
+              }
+            }
+          ]
+        }
+      },
+
+      myBarData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "Data One",
             backgroundColor: [
-              '#ff6384',
-              '#36a2eb',
-              '#cc65fe',
-              '#ffce56',
-              'red',
-              'green',
-              'blue'
+              "#ff6384",
+              "#36a2eb",
+              "#cc65fe",
+              "#ffce56",
+              "red",
+              "green",
+              "blue"
             ],
-            borderColor: '#fff',
+            data: [40, 39, 20, 40, 39, 80, 40]
+          },
+          {
+            label: "Data Two",
+            borderColor: "#79f879",
+            backgroundColor: [
+              "#ffce56",
+              "red",
+              "green",
+              "blue",
+              "#ff6384",
+              "#36a2eb",
+              "#cc65fe"
+            ],
+            data: [39, 10, 40, 39, 80, 100, 22]
+          },
+          {
+            label: "Data Three",
+            borderColor: "#79f879",
+            backgroundColor: [
+              "blue",
+              "#ff6384",
+              "#36a2eb",
+              "red",
+              "green",
+              "#cc65fe",
+              "#ffce56"
+            ],
+            data: [29, 40, 10, 69, 10, 30, 92]
+          }
+        ]
+      },
+      myBarOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "My X-axis Label"
+              }
+            }
+          ],
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                stepSize: 5
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "My Y-axis Label"
+              }
+            }
+          ]
+        }
+      },
+
+      myPieData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: [
+              "#ff6384",
+              "#36a2eb",
+              "#cc65fe",
+              "#ffce56",
+              "red",
+              "green",
+              "blue"
+            ],
+            data: [40, 39, 10, 40, 39, 80, 40]
+          }
+        ]
+      },
+      myPieOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
+
+      myDoughnutData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: [
+              "#ff6384",
+              "#36a2eb",
+              "#cc65fe",
+              "#ffce56",
+              "red",
+              "green",
+              "blue"
+            ],
+            borderColor: "#fff",
             borderWidth: 2,
             data: [40, 39, 10, 40, 39, 80, 40]
           }
         ]
-      }"
-      :options="{ responsive: true, maintainAspectRatio: false }"
-    />
+      },
+      myDoughnutOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
 
-    <hr />
-    <h1>BubbleChart</h1>
-    <BubbleChart
-      :data="{
+      myBubbleData: {
         labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
         ],
         datasets: [
           {
-            label: 'Data One',
-            hoverBackgroundColor: '#ff6384',
+            label: "Data One",
+            hoverBackgroundColor: "#ff6384",
             hoverRadius: 2,
             backgroundColor: [
-              '#ff6384',
-              '#36a2eb',
-              '#cc65fe',
-              '#ffce56',
-              'red',
-              'green',
-              'blue'
+              "#ff6384",
+              "#36a2eb",
+              "#cc65fe",
+              "#ffce56",
+              "red",
+              "green",
+              "blue"
             ],
             data: [
               {
@@ -258,35 +337,34 @@
             ]
           }
         ]
-      }"
-      :options="{ responsive: true, maintainAspectRatio: false }"
-    />
+      },
+      myBubbleOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
 
-    <hr />
-    <h1>ScatterChart</h1>
-    <ScatterChart
-      :data="{
+      myScatterData: {
         labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
         ],
         datasets: [
           {
-            label: 'Data One',
+            label: "Data One",
             backgroundColor: [
-              '#ff6384',
-              '#36a2eb',
-              '#cc65fe',
-              '#ffce56',
-              'green',
-              'blue'
+              "#ff6384",
+              "#36a2eb",
+              "#cc65fe",
+              "#ffce56",
+              "green",
+              "blue"
             ],
-            borderColor: '#fff',
+            borderColor: "#fff",
             borderWidth: 2,
             pointRadius: 10,
             pointHoverRadius: 15,
@@ -309,45 +387,25 @@
               },
               {
                 x: 50,
-                y: 80
+                y: 20
               }
             ]
           }
         ]
-      }"
-      :options="{
+      },
+      myScatterOptions: {
         responsive: true,
         maintainAspectRatio: false,
         scales: {
           xAxes: [
             {
-              type: 'linear',
-              position: 'bottom'
+              type: "linear",
+              position: "bottom"
             }
           ]
         }
-      }"
-    />
-  </div>
-</template>
-
-<script>
-import LineChart from "./components/Charts/Chartjs/LineChart.vue";
-import BarChart from "./components/Charts/Chartjs/BarChart.vue";
-import PieChart from "./components/Charts/Chartjs/PieChart.vue";
-import DoughnutChart from "./components/Charts/Chartjs/DoughnutChart.vue";
-import BubbleChart from "./components/Charts/Chartjs/BubbleChart.vue";
-import ScatterChart from "./components/Charts/Chartjs/ScatterChart.vue";
-
-export default {
-  name: "app",
-  components: {
-    LineChart,
-    BarChart,
-    PieChart,
-    DoughnutChart,
-    BubbleChart,
-    ScatterChart
+      }
+    };
   }
 };
 </script>
